70277d274348f0891ecec35225de66c7a30eeee3
Diana Atas <diana.atas@trademe.co.nz>
2023-06-10 18:35:04 +1200

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Diana Atas on 10/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Diana Atas on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
